628866a8ebff27edcbc4be97942b41999ab4c967
William Hayward <haywardwilliam1996@gmail.com>
2018-05-23 14:06:52 +1000

    +Added implode script

---
+++ implode.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import json
+import collections
+# Usage: `implode.py [input folder] [output file]`
+def main():
+    spellsPath = sys.argv[1]
+    output = sys.argv[2]
+    spellNames = os.listdir(spellsPath)
+    spellNames.sort()
+    spells = []
+    for spellName in spellNames:
+        name = spellName.replace('%2F', '/')
+        spellFile = open(spellsPath + spellName)
+        spells.append(json.load(spellFile, object_pairs_hook=collections.OrderedDict))
+
+    compiled = open(output, 'w+')
+    json.dump(spells, compiled, indent=4)
+
+if __name__ == "__main__":
+    main()
